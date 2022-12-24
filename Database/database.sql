7f08c65cad99857412c144a6c31fa6030d8aad90
Abderafii Abdou <107323923+abderafii@users.noreply.github.com>
2022-12-24 22:42:03 +0100

    Update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -1,20 +1,11 @@
 -- we start by creating the different tables that we need, these tables are hosted online on railway to allow for team modifications
 --Not all tables are used due to lack of time and expertise concerning programing languages
-create table Monthly_Summary(
- MonthID    Serial PRIMARY KEY,
- MonthName  varchar(35) not null
-);
 
-create table Monthly_Summary_Line(
- MonthID int,
- LineID  int,
+create table Profit_Line(
+ ProfitLineID  serial PRIMARY KEY,
+ createdate date,
  productID   int,
- MonthlyQuantity     int not null DEFAULT 0,
- MonthlyTotalProfit     int not null Default 0,
- PRIMARY KEY (MonthID, LineID),
- UNIQUE(MonthID, productID),
- CONSTRAINT Has FOREIGN KEY (MonthID) REFERENCES Monthly_Summary(MonthID)
-                      ON UPDATE CASCADE ON DELETE CASCADE,
+ ProdTotalProfit     int not null Default 0,
  CONSTRAINT Appears FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
                       ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -188,3 +179,102 @@ $$;
 
 Create trigger cusbalancetrig 
 after insert on line for each row execute function trig4();
+
+--create trigger in order to create a row in the invoice table since the invoice table does not 
+--take in consideration the invoices created by his subtypes.
+create or replace function trig5() returns trigger
+language plpgsql
+as 
+$$ 
+Begin
+Insert into invoice (invoiceId, invoicetype) values (new.invoiceid, new.invoicetype);
+return new;
+End;
+$$;
+
+Create trigger cusinv
+after insert on Customer_Invoice for each row execute function trig5();
+
+--create a trigger to calculate the profit for each product;
+create or replace function trig6() returns trigger
+language plpgsql
+as 
+$$ 
+declare 
+total int;
+Begin
+total = (new.LineHistoricalPrice - (select ProductAcquisitionPrice from product where productid=new.productid)) * new.LineQuantity;
+Insert into Profit_Line(createdate ,productid, ProdTotalProfit) values (current_date ,new.productid, total);
+return new;
+End;
+$$;
+
+Create trigger createmonthsumline
+after insert on Line for each row execute function trig6();
+
+--create trigger to update quantity on hand
+create or replace function trig7() returns trigger
+language plpgsql
+as 
+$$ 
+Begin
+Update Product
+set ProductQuantityOnHand = ProductQuantityOnHand - new.LineQuantity
+where productid = new.productid;
+return new;
+End;
+$$;
+
+Create trigger updatequantonhand
+after insert on Line for each row execute function trig7();
+
+--create trigger to check the quantity on hand and the min quantity on hand
+create or replace function trig8() returns trigger
+language plpgsql
+as 
+$$ 
+Begin
+IF new.ProductQuantityOnHand < new.ProductMinQuantityOnHand THEN
+   Update product 
+   set ProductQuantityOnHand = new.ProductQuantityOnHand + 5 where productID = new.productID;
+   update supplier 
+   set DebtToSup = DebtToSup + 5*new.ProductAcquisitionPrice where SupID = new.SupID;
+END IF;
+return new;
+End;
+$$;
+
+Create trigger orderprod
+after update on product for each row execute function trig8();
+
+--Populating the Database
+Insert into product 
+(ProductDescription, supid, ProductCurrentPrice, ProductAcquisitionPrice, ProductQuantityOnHand,
+ ProductMinQuantityOnHand, ProductDiscount) values 
+ ('Head Lamp for Dacia', 5, 450, 400, 15, 5, 00),
+ ('Front Wiper 650mm', 2, 100, 70, 12, 5, 00),
+ ('Kit Timing Belt Dacia', 3, 800, 650, 10, 5, 00),
+ ('Dacia Dokker Oil Filter', 2, 180, 150, 12, 5, 00),
+ ('Diesel Filter RENAULT Clio', 4, 169, 140, 14, 5, 00),
+ ('Helix Ultra 5w 40', 1, 200, 190, 15, 5, 00),
+ ('Air Filter Clio', 3, 80, 60, 20, 5, 00),
+ ('Activa 9000 5w-40 1 Liter', 6, 40, 20, 20, 5, 00);
+
+Insert into supplier
+(supname, supaddress, supphone, debttosup) values
+ ('Filtron', 'Casablanca Hay Riad', '0625553696'),
+ ('Jiangsu Reshine', 'China Beijing', '+8625368965'), 
+ ('Total', 'Casablanca Derb Ghalef', '0525553265'),
+ ('Shell', 'Casablanca Anfa Place', '0525557898'),
+ ('Renault', 'Casablanca Ain Sbaa', '0525553654'),
+ ('Bosch', 'Casablanca Maarif', '0525558744');
+
+Insert into Representative
+(RepFname, RepLname, RepPhone, supid) values
+ ('Ahmed', 'Zianni', '0637384185', 1),
+ ('Hamza', 'Arafat', '0658693354', 2),
+ ('Hamid', 'Lhlou', '0748996355', 3),
+ ('Hamada', 'Laala', '0698875521', 4),
+ ('Ali', 'Benhadou', '0635142589', 5),
+ ('Riad', 'Senhaji', '0635994885', 6);
+ 
